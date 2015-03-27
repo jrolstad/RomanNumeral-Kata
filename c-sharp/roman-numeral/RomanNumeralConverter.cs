b68f24067f148b33f7adac3bb3646054f4892fbb
Josh Rolstad <jrolstad@gmail.com>
2015-03-26 20:09:50 -0700

    Simple implementation of conversion

--- c-sharp/roman-numeral/RomanNumeralConverter.cs
+++ c-sharp/roman-numeral/RomanNumeralConverter.cs
@@ -1,10 +1,23 @@
-﻿namespace roman_numeral
+﻿using System.Collections.Generic;
+
+namespace roman_numeral
 {
     public class RomanNumeralConverter
     {
         public int Convert(string romanNumeral)
         {
-            return 1;
+            var values = new Dictionary<string, int>
+            {
+                {"I", 1},
+                {"V", 5},
+                {"X", 10},
+                {"L", 50},
+                {"C", 100},
+                {"D", 500},
+                {"M", 1000},
+            };
+
+            return values[romanNumeral];
         }
     }
 }
